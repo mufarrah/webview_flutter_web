9207d7e91c5775268130213d746a543a70960eb1
Abdulrahman Mufarrah <a.mufarrah@gmail.com>
2024-07-31 11:52:35 +0300

    feat:V.1.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'register_web_webview_stub.dart'
-    if (dart.library.html) "register_web_webview.dart";
+import 'package:web_view/iframeView.dart';
 
-void main() {
-  registerWebViewWebImplimentation();
-  runApp(const MyApp());
+void main() async {
+  runApp(IFrameTesterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class IFrameTesterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Webview flutter web',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Webview flutter web'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+      home: Scaffold(
         appBar: AppBar(
-          title: Text(widget.title),
+          title: Text('Webview Example'),
+          centerTitle: true,
         ),
-        body: Container(
-          child: Row(
-            children: const [
-              Expanded(
-                child: WebView(
-                  initialUrl: "https://flutter.dev",
-                ),
+        body: const Row(
+          children: [
+            Expanded(
+              child: IframeView(
+                source: 'https://flutter.dev',
               ),
-              Expanded(
-                child: WebView(
-                  initialUrl: "https://www.squarespace.com/website-design",
-                ),
+            ),
+            Expanded(
+              child: IframeView(
+                source: 'https://www.squarespace.com/website-design',
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+      theme: ThemeData.light(),
+    );
   }
 }
